713fd2f0256d1e3cc0ea954c979db78de7d649a5
Ciarán Malone <ciaranmalone27@gmail.com>
2022-06-24 20:33:55 +0100

    Gamemanager

---
+++ Assets/_Scripts/GameManager/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _GM;
+    public static GameManager GM { get { return _GM; } }
+    public int Health = 100;
+
+    public int Level = 0;
+
+    void Awake()
+    {
+        if (_GM != null && _GM != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _GM = this;
+        }
+    }
+
+    internal void ChangeHeath(int health)
+    {
+        Health += health;
+    }
+}
