d0b754fe36702d8e495364340ba04f347253d9b3
Ciarán Malone <ciaranmalone27@gmail.com>
2022-06-25 19:01:45 +0100

    Player prefabs pushed for real no take backszies
    I love ass and I am Ciaran (real)

--- Assets/playerSwapSprites.cs
+++ Assets/playerSwapSprites.cs
@@ -7,23 +7,24 @@ public class playerSwapSprites : MonoBehaviour
    [SerializeField] RuntimeAnimatorController facingForward;
     [SerializeField] RuntimeAnimatorController sideOn;
     [SerializeField] RuntimeAnimatorController facingBackward;
+    [SerializeField] GameObject childWithSprites;
 
     public void whichDirectionShouldBeFacing(Vector3 t_direction)
     {
         if (t_direction.x > 0) right();
         else if (t_direction.x < 0) left();
-        if (t_direction.z > 0 ) this.GetComponentInChildren<Animator>().runtimeAnimatorController = facingBackward;
-        else if (t_direction.z < 0 ) this.GetComponentInChildren<Animator>().runtimeAnimatorController = facingForward;
+        if (t_direction.z > 0 ) childWithSprites.GetComponent<Animator>().runtimeAnimatorController = facingBackward;
+        else if (t_direction.z < 0 ) childWithSprites.GetComponent<Animator>().runtimeAnimatorController = facingForward;
        
     }
     public void left()
     {
-        this.GetComponentInChildren<Animator>().runtimeAnimatorController = sideOn;
-        this.GetComponentsInChildren<SpriteRenderer>()[1].flipX = true;
+        childWithSprites.GetComponent<Animator>().runtimeAnimatorController = sideOn;
+        childWithSprites.GetComponent<SpriteRenderer>().flipX = true;
     }
     public void right()
     {
-        this.GetComponentInChildren<Animator>().runtimeAnimatorController = sideOn;
-        this.GetComponentsInChildren<SpriteRenderer>()[1].flipX = false;
+        childWithSprites.GetComponent<Animator>().runtimeAnimatorController = sideOn;
+        childWithSprites.GetComponent<SpriteRenderer>().flipX = false;
     }
 }
