1586d1113f989a352fe70b7f64ab67134636e582
Ciarán Malone <ciaranmalone27@gmail.com>
2022-06-25 02:07:53 +0100

    The Enemies now hurt you

--- Assets/_Scripts/Enemies/EnemyCollider.cs
+++ Assets/_Scripts/Enemies/EnemyCollider.cs
@@ -5,12 +5,55 @@ using UnityEngine;
 public class EnemyCollider : MonoBehaviour
 {
     [SerializeField] int HealthLost = 1;
+    [Tooltip("In Seconds")]
+    [SerializeField] float dotDamage = .5f;
+    [SerializeField] AudioClip hitSound;
+    AudioSource audioSource;
+
+    private bool inRange = false;
+    private float timer = 0;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if (inRange)
+        {
+            timer += Time.deltaTime;
+            if (timer >= dotDamage)
+            {
+                Attack();
+                timer = 0;
+            }
+        }  
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.GM.ChangeHeath(-1*HealthLost);
+            Attack();
+            inRange = true;
+            timer = 0;
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = false;
+            timer = 0;
+        }
+    }
+
+    private void Attack()
+    {
+        GameManager.GM.ChangeHeath(-1 * HealthLost);
+        audioSource.PlayOneShot(hitSound);
+    }
+
 }
--- Assets/_Scripts/GameManager/GameManager.cs
+++ Assets/_Scripts/GameManager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -37,6 +38,7 @@ public class GameManager : MonoBehaviour
     private void Died()
     {
         Debug.Log("DIED DIED DIED DIED DIED");
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
     }
 
     internal void AddEnemy(GameObject enemy)
