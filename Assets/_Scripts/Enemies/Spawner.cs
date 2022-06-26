d2636b6025af86c5862d0d2b234295008943e13a
Ciarán Malone <ciaranmalone27@gmail.com>
2022-06-26 15:09:10 +0100

    Spawner Script updated

--- Assets/_Scripts/Enemies/Spawner.cs
+++ Assets/_Scripts/Enemies/Spawner.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] int level = 4;
     [SerializeField] List<SpawnChance> spawnees;
-
-    [SerializeField] float min = 1f;
-    [SerializeField] float max = 2f;
+    [SerializeField] List<SpawnPoint> spawnPoints;
+    [SerializeField] int MaxSpawn = 100;
+    [Header("Random Min Max")]
+    [SerializeField] float ranMin = 1f;
+    [SerializeField] float ranMax = 2f;
     void Start()
     {
        /* spawnees.Sort(SpawnChance.)*/
@@ -22,18 +25,35 @@ public class Spawner : MonoBehaviour
 
         while (GameManager.GM.currentLevel == level)
         {
-            yield return new WaitForSeconds(Random.Range(min, max));
+            yield return new WaitForSeconds(Random.Range(ranMin, ranMax));
             float ran = Random.Range(0f, 1f);
 
             foreach (SpawnChance spawnee in spawnees)
             {
                 if (spawnee.spawnChange >= ran)
                 {
-                    Instantiate(spawnee.spawnee, transform.position, transform.rotation);
+                    SpawnPoint randomSpawn = spawnPoints[Random.Range(0, spawnPoints.Count)];
+
+                    Instantiate(spawnee.spawnee, 
+                        (randomSpawn.point + (Random.insideUnitSphere * randomSpawn.radius)).With(y: randomSpawn.point.y), 
+                        transform.rotation);
+
                     break;
                 }
             }
         }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        foreach (var spawn in spawnPoints)
+        {
+            Gizmos.DrawCube(spawn.point, new Vector3(1, 1, 1));
+            Gizmos.DrawWireSphere(spawn.point, spawn.radius);
+        }
+
+
     }
 
     [System.Serializable]
@@ -45,4 +65,12 @@ public class Spawner : MonoBehaviour
         [SerializeField] internal float spawnChange;
     }
 
+    [System.Serializable]
+    private class SpawnPoint
+    {
+        [SerializeField] 
+        internal Vector3 point;
+        [SerializeField] 
+        internal float radius = 10; 
+    }
 }
